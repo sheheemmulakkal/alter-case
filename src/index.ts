11491a660a010415fe1c78c8901d3d08979d9498
sheheemmulakkal <shaimonsheheem@gmail.com>
2024-01-27 09:47:00 +0530

    feat: Export toKebabCase function
    This commit exports the 	oKebabCase function, making it accessible for use in other parts of the project. The exported function enhances text formatting capabilities by converting sentences, paragraphs, or words to kebab case.

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./alter-case/upper-case/upper-case";
 export * from "./alter-case/lower-case/lower-case";
 export * from "./alter-case/title-case/title-case";
 export * from "./alter-case/sentence-case/sentence-case";
+export * from "./alter-case/kebab-case/kebab-case";
